2cc46732fac58617c0d020d9480884ccfb15a32e
Durai23 <khcy5dnr@nottingham.edu.my>
2017-11-13 16:25:18 +0800

    Added seekbar and play button with methods

--- app/src/main/java/com/example/durai23/musicplayerapp/MP3Player.java
+++ app/src/main/java/com/example/durai23/musicplayerapp/MP3Player.java
@@ -96,4 +96,8 @@ public class MP3Player {
             mediaPlayer = null;
         }
     }
+
+    public void seek(int seconds){
+        mediaPlayer.seekTo(seconds);
+    }
 }
--- app/src/main/java/com/example/durai23/musicplayerapp/MainActivity.java
+++ app/src/main/java/com/example/durai23/musicplayerapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.durai23.musicplayerapp;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Environment;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -11,19 +12,35 @@ import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.File;
 import java.io.FilenameFilter;
 import java.util.ArrayList;
 import java.util.List;
+import android.os.Handler;
+
+import java.util.concurrent.TimeUnit;
+import java.util.logging.LogRecord;
 
 public class MainActivity extends AppCompatActivity {
 
+    static final int READ_STORAGE_REQUEST_CODE = 1;
+
     private MP3Player mp3Player = new MP3Player();
     private ArrayList<File> songList = new ArrayList<File>();
-    private boolean flag = false;
+    private SeekBar seekBar;
+    private TextView currentTime;
+    private TextView totalDuration;
+    private ImageButton playButton;
+    private ImageButton rewindButton;
+    private ImageButton forwardButton;
+    private Handler mHandler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,26 +48,46 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //check for permission
-        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)
+        if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)
                 != PackageManager.PERMISSION_GRANTED) {
 
             // Request the permission.
             ActivityCompat.requestPermissions(MainActivity.this,
-                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, READ_STORAGE_REQUEST_CODE);
+        }
+        else{
+            loadSongs_listView();
         }
 
+        seekBar = (SeekBar) findViewById(R.id.progressBar);
+        currentTime = (TextView) findViewById(R.id.currentTime);
+        totalDuration = (TextView) findViewById(R.id.totalDuration);
+        playButton = (ImageButton) findViewById(R.id.play_button);
+        rewindButton = (ImageButton) findViewById(R.id.back_button);
+        forwardButton = (ImageButton) findViewById(R.id.forward_button);
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch(requestCode){
+            case READ_STORAGE_REQUEST_CODE:
+                loadSongs_listView();
+                break;
+            default:
+                Toast.makeText(MainActivity.this,"ERROR",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void loadSongs_listView(){
+
         final ListView listView = (ListView)findViewById(R.id.listView);
-
         File musicDir = new File(Environment.getExternalStorageDirectory().getPath());
-
-        Toast.makeText(this,musicDir.toString(),Toast.LENGTH_SHORT).show();
-
         scanMP3_Files(musicDir);
-
         String[] songName_list = new String[songList.size()];
 
         for(int i = 0; i < songName_list.length; i++){
-            songName_list[i] = songList.get(i).getName();
+            songName_list[i] = songList.get(i).getName().substring(0, songList.get(i).getName().length()-4);
         }
 
         listView.setAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,songName_list));
@@ -59,11 +96,51 @@ public class MainActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> myAdapter, View myView, int myItemInt,
                                     long mylng) {
                 File selectedFromList = songList.get(myItemInt);
-                Toast.makeText(MainActivity.this,selectedFromList.getName().toString(),Toast.LENGTH_SHORT).show();
-
+                mp3Player.stop();
                 mp3Player.load(selectedFromList.getPath());
+                playButton.setImageResource(R.drawable.pause_button);
+                seekBar.setMax(mp3Player.getDuration()/1000);
+
+                totalDuration.setText(String.format("%02d:%02d",
+                        TimeUnit.MILLISECONDS.toMinutes(mp3Player.getDuration())% TimeUnit.HOURS.toMinutes(1),
+                        TimeUnit.MILLISECONDS.toSeconds(mp3Player.getDuration())% TimeUnit.MINUTES.toSeconds(1)));
+
+                MainActivity.this.runOnUiThread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        if(mp3Player != null){
+                            int mCurrentPosition = mp3Player.getProgress() / 1000;
+                            seekBar.setProgress(mCurrentPosition);
+
+                            currentTime.setText(String.format("%02d:%02d",
+                                    TimeUnit.MILLISECONDS.toMinutes(mp3Player.getProgress())% TimeUnit.HOURS.toMinutes(1),
+                                    TimeUnit.MILLISECONDS.toSeconds(mp3Player.getProgress())% TimeUnit.MINUTES.toSeconds(1)));
+                        }
+                        mHandler.postDelayed(this, 1000);
+                    }
+                });
+
+                seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
+                    public void onStopTrackingTouch(SeekBar seekBar) {
+
+                    }
+
+                    public void onStartTrackingTouch(SeekBar seekBar) {
+
+                    }
+
+                    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                        if(mp3Player != null && fromUser){
+                            mp3Player.seek(progress * 1000);
+                        }
+                    }
+                });
+
             }
         });
+
+        Toast.makeText(MainActivity.this,"Total MP3 songs: "+ Integer.toString(songList.size()),Toast.LENGTH_SHORT).show();
     }
 
     private void scanMP3_Files(File directory){
@@ -87,4 +164,20 @@ public class MainActivity extends AppCompatActivity {
             songList.add(file);
         }
     }
+
+    public void play_pauseOnClick(View view){
+        if(mp3Player.getState() == MP3Player.MP3PlayerState.PLAYING){
+            playButton.setImageResource(R.drawable.play_button);
+            mp3Player.pause();
+        }
+        else if(mp3Player.getState() == MP3Player.MP3PlayerState.PAUSED){
+            playButton.setImageResource(R.drawable.pause_button);
+            mp3Player.play();
+        }
+        else if(mp3Player.getState() == MP3Player.MP3PlayerState.STOPPED){
+            Toast.makeText(MainActivity.this,"Choose a song to play first.",Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
 }
